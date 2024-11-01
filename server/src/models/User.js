182253d465d5e20c146de4227f4f2ee8deb60d2e
jdaly3411 <jdaly3411@gmail.com>
2024-11-01 12:53:10 +0000

    Added the models

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,3 +1,34 @@
 // create your User model here
 const { DataTypes } = require("sequelize");
 const sequelize = require("../db/");
+
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: DataTypes.STRING,
+});
+const Deck = sequelize.define("Deck", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: DataTypes.STRING,
+  xp: DataTypes.INTEGER,
+});
+const Card = sequelize.define("Card", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: DataTypes.STRING,
+  mojo: DataTypes.INTEGER,
+  stamina: DataTypes.INTEGER,
+  imgUrl: DataTypes.STRING,
+});
+
+module.exports = { User, Deck };
